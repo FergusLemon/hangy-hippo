fa9b212bffae55acdb6884b9ca926802c794c258
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-11 14:55:07 +0000

    Write first test to check that a div with a class name exists.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders a div element with the className prop set to be an item card', () => {
+  const wrapper = shallow(<App />);
+
+  expect(
+    wrapper
+      .find('div')
+      .findWhere(e => e.props().className === 'list-item-card').length
+      ).toBe(1);
 });
