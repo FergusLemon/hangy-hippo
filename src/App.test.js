850e7caf63fedba490634eb53e2ac4c8ca35a884
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-14 18:00:07 +0000

    Test and implement basic functionality of the recipe list item and add some styling to the component.

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 const getElement = wrapper => elementType => classToSearchFor => (
   wrapper
   .find(elementType)
-  .findWhere(e => e.props().className && e.props().className.indexOf(classToSearchFor) !== -1)
+  .filterWhere(n => n.hasClass(classToSearchFor))
 );
 
 it('renders without crashing', () => {
@@ -22,10 +22,48 @@ describe('Recipe List Item', () => {
   });
 
   it('should display the image supplied by the props', () => {
-    const testAlt = `I'm Batman`,
+    const testAlt = `I'm a Lime`,
           testImage = 'https://spoonacular.com/recipeImages/Easy---Delish--Apple-Crumble-641803.jpg',
-          wrapper = shallow(<App title={ testAlt } image={ testImage }/>);
-    expect(getElement(wrapper)('img')('recipe-list-item-image').props().alt).toEqual(testAlt);
-    expect(getElement(wrapper)('img')('recipe-list-item-image').props().src).toEqual(testImage);
+          wrapper = shallow(<App title={testAlt} image={testImage} />);
+    expect(getElement(wrapper)('img')('recipe-list-item-image').props().alt)
+      .toEqual(testAlt);
+    expect(getElement(wrapper)('img')('recipe-list-item-image').props().src)
+      .toEqual(testImage);
+  });
+
+  it('should display a title', () => {
+    const testTitle = `I'm a Lime`,
+          wrapper = shallow(<App title={testTitle} />);
+    expect(getElement(wrapper)('div')('recipe-list-item-title').text())
+      .toEqual(testTitle);
+  });
+
+  it('should show the number of ingredients used', () => {
+    const testUsedIngredientCount = 1,
+          wrapper = shallow(<App usedIngredientCount={testUsedIngredientCount} />);
+    expect(getElement(wrapper)('div')('recipe-list-item-ingredient-count')
+      .text().indexOf(`${testUsedIngredientCount}`)).not.toEqual(-1);
+  });
+
+  it("should display the word 'all' when all ingredients have been used and not the number", () => {
+    const testUsedIngredientCount = 3,
+          testMissedIngredientCount = 0,
+          wrapper = shallow(<App usedIngredientCount={testUsedIngredientCount} missedIngredientCount={testMissedIngredientCount} />);
+    expect(getElement(wrapper)('div')('recipe-list-item-ingredient-count')
+      .text().indexOf('all')).not.toEqual(-1);
+  });
+
+  it('should display a missing ingredient count if there are missing ingredients', () => {
+    const testMissedIngredientCount = 3,
+          wrapper = shallow(<App missedIngredientCount={testMissedIngredientCount} />);
+    expect(getElement(wrapper)('div')('recipe-list-item-missed-ingredient-count')
+      .text().indexOf(`${testMissedIngredientCount}`)).not.toEqual(-1);
+  });
+
+  it('should not display a missing count if there are no missing ingredients', () => {
+    const testMissedIngredientCount = 0,
+          wrapper = shallow(<App missedIngredientCount={testMissedIngredientCount} />);
+    expect(getElement(wrapper)('div')('recipe-list-item-missed-ingredient-count')
+      .exists()).toEqual(false);
   });
 });
